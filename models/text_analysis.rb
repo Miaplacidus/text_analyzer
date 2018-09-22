3105c804765d02e4d5057724e879def5843919cf
Ifu Aniemeka <ianiemeka@gmail.com>
2018-09-22 15:56:44 -0500

    Downcase file content words

--- models/text_analysis.rb
+++ models/text_analysis.rb
@@ -35,8 +35,11 @@ class TextAnalysis < ActiveRecord::Base
     end
 
     def calculate_frequencies
-        file_content.split(/[^a-zA-Z0-9_']/).each do |word|
+        file_content.split(/[^a-zA-Z0-9_']+/).each do |word|
             next if (exclude_stopwords? && is_stopword?(word))
+
+            word.downcase!
+
             stem =
             case
             when word.match(/(\w*)(ies\z)/)
